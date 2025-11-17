d11db72b680b054e0415dfbab769709b7333c735
ayathree <nobanitaayathree333@gmail.com>
2025-11-17 14:09:01 +0600

    solving 6th problem

--- solution.ts
+++ solution.ts
@@ -60,6 +60,19 @@ function filterActiveUsers (items:UserItem[]):UserItem[]{
 
 
 //Problem 6
+interface Book{
+    title:string;
+    author:string;
+    publishedYear:number;
+    isAvailable:boolean;
+}
+function printBookDetails(book:Book):void{
+    const avaibility = book.isAvailable? 'Yes' : 'No';
+    console.log(`Title:${book.title}, Author:${book.author}, Published:${book.publishedYear}, Available:${avaibility}`)
+
+}
+
+//Problem 7
 
 
 
